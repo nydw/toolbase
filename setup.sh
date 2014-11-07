e78ec9d4b0f20d04ff7026b3134bb60cdf3a1784
lgx <lgx@debian.lgx-vpc.com>
2014-11-08 01:27:26 +0800

    new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,48 @@
+
+sudo apt-get install build-essential 
+sudo apt-get install xorg-dev 
+sudo apt-get install libgtk2.0-dev 
+sudo apt-get install texinfo 
+sudo apt-get install libjpeg-dev 
+sudo apt-get install libgif-dev 
+sudo apt-get install libtiff4-dev 
+sudo apt-get install libncurses5-dev
+sudo apt-get install binutils-doc 
+sudo apt-get install cpp-doc 
+sudo apt-get install gcc-4.1-doc 
+sudo apt-get install gcc-doc 
+sudo apt-get install glibc-doc 
+sudo apt-get install libstdc++6-4.1-doc 
+sudo apt-get install stl-manual 
+sudo apt-get install cpp-4.1-doc 
+sudo apt-get install manpages 
+sudo apt-get install manpages-dev 
+sudo apt-get install openjdk-7-jdk 
+sudo apt-get install openjdk-7-jre
+sudo apt-get install openjdk-7-dbg
+sudo apt-get install openjdk-7-lib
+sudo apt-get install openjdk-7-doc
+sudo apt-get install ddd gftp rar
+sudo apt-get install bless hexedit
+apt-get install bison gawk libc6-dev
+sudo apt-get install automake autoconf
+sudo apt-get install libtool autogen
+sudo apt-get install indent vim git
+sudo apt-get install mercurial cvs
+sudo apt-get install subversion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

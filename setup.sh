6fa277d41257313f1b3220c6b4c062c90f344943
nydw <nydw.lgx@qq.com>
2014-12-03 02:48:53 +0800

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,35 +1,37 @@
 
-sudo apt-get install build-essential 
-sudo apt-get install xorg-dev 
-sudo apt-get install libgtk2.0-dev 
-sudo apt-get install texinfo 
-sudo apt-get install libjpeg-dev 
-sudo apt-get install libgif-dev 
-sudo apt-get install libtiff4-dev 
-sudo apt-get install libncurses5-dev
-sudo apt-get install binutils-doc 
-sudo apt-get install cpp-doc 
-sudo apt-get install gcc-4.1-doc 
-sudo apt-get install gcc-doc 
-sudo apt-get install glibc-doc 
-sudo apt-get install libstdc++6-4.1-doc 
-sudo apt-get install stl-manual 
-sudo apt-get install cpp-4.1-doc 
-sudo apt-get install manpages 
-sudo apt-get install manpages-dev 
-sudo apt-get install openjdk-7-jdk 
-sudo apt-get install openjdk-7-jre
-sudo apt-get install openjdk-7-dbg
-sudo apt-get install openjdk-7-lib
-sudo apt-get install openjdk-7-doc
-sudo apt-get install ddd gftp rar
-sudo apt-get install bless hexedit
-apt-get install bison gawk libc6-dev
-sudo apt-get install automake autoconf
-sudo apt-get install libtool autogen
-sudo apt-get install indent vim git
-sudo apt-get install mercurial cvs
-sudo apt-get install subversion
+sudo apt-get install build-essential -y 
+sudo apt-get install xorg-dev -y
+sudo apt-get install libgtk2.0-dev  -y
+sudo apt-get install texinfo  -y
+sudo apt-get install libjpeg-dev -y 
+sudo apt-get install libgif-dev  -y
+sudo apt-get install libtiff4-dev  -y
+sudo apt-get install libncurses5-dev -y
+sudo apt-get install binutils-doc  -y
+sudo apt-get install cpp-doc  -y
+sudo apt-get install gcc-4.1-doc -y 
+sudo apt-get install gcc-doc  -y
+sudo apt-get install glibc-doc  -y
+sudo apt-get install libstdc++6-4.1-doc  -y
+sudo apt-get install stl-manual  -y
+sudo apt-get install cpp-4.1-doc  -y
+sudo apt-get install manpages  -y
+sudo apt-get install manpages-dev -y 
+sudo apt-get install openjdk-7-jdk  -y
+sudo apt-get install openjdk-7-jre -y
+sudo apt-get install openjdk-7-dbg -y
+sudo apt-get install openjdk-7-lib -y
+sudo apt-get install openjdk-7-doc -y
+sudo apt-get install ddd gftp rar -y
+sudo apt-get install bless hexedit -y
+apt-get install bison gawk libc6-dev -y
+sudo apt-get install automake autoconf -y
+sudo apt-get install libtool autogen -y
+sudo apt-get install indent vim git -y
+sudo apt-get install ctags id-utils -y
+sudo apt-get install mercurial cvs -y
+sudo apt-get install subversion -y
+sudo apt-get install cscope sed -y
 
 
 
